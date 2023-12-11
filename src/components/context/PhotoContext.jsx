114dd6838996356f828d4cb82d4360a5788d2b2c
Constanza Valenzuela <kokony.vm@gmail.com>
2023-12-10 21:54:21 -0300

    fix url photos

--- src/components/context/PhotoContext.jsx
+++ src/components/context/PhotoContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import axios from 'axios';
 export const PhotosContext = createContext();
 
-const URL = '/photos.json';
+const URL = '/NaturalPic/photos.json';
 
 const PhotosProvider = ({children}) => {
     const [photos, setPhotos] = useState([]);
